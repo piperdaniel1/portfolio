e9265bd99c01ec979c48b81619af4cfa4d3a8da4
Daniel Piper <piperdaniel1@gmail.com>
2022-07-18 10:38:54 -0700

    sidebar opens/closes

--- react-src/src/components/Sidebar.js
+++ react-src/src/components/Sidebar.js
@@ -10,11 +10,28 @@ function Sidebar() {
   }, [isOpen]);
 
   return (
-    <div class="side-header-bar">
-      <div class="sidebar-hamburger hamburger-icon" onClick={() => setIsOpen(!isOpen)}>
-        <div class="hamburger-icon-line"></div>
-        <div class="hamburger-icon-line"></div>
-        <div class="hamburger-icon-line"></div>
+    <div class={isOpen ? 'side-header-bar active':'side-header-bar inactive'}>
+      <div class="hamburger-wrapper">
+        <div class="sidebar-hamburger hamburger-icon" onClick={() => setIsOpen(!isOpen)}>
+          <div class="hamburger-icon-line"></div>
+          <div class="hamburger-icon-line"></div>
+          <div class="hamburger-icon-line"></div>
+        </div>
+      </div>
+
+      <div class={isOpen ? 'sidebar-nav-buttons active':'sidebar-nav-buttons inactive'}>
+        <div class="sidebar-nav-button">
+          <p class="sidebar-nav-text-button"> Portfolio </p>
+        </div>
+        <div class="sidebar-nav-button">
+          <p class="sidebar-nav-text-button"> About me </p>
+        </div>
+        <div class="sidebar-nav-button">
+          <p class="sidebar-nav-text-button"> References </p>
+        </div>
+        <div class="sidebar-nav-button">
+          <p class="sidebar-nav-text-button"> Contact </p>
+        </div>
       </div>
     </div>
   );
