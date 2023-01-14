046374e0aa7574db619d327344cbe41678efb96f
Daniel Piper <piperdaniel1@gmail.com>
2023-01-13 17:29:46 -0800

    /query/fen route

--- server/main.go
+++ server/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+type QueryFen struct {
+	Fen  string // FEN string from board
+	Time int32  // Time that the computer should think
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
+	})
+	http.HandleFunc("/query/fen", func(w http.ResponseWriter, r *http.Request) {
+		// Parse body into JSON
+		var query QueryFen
+		json.NewDecoder(r.Body).Decode(&query)
+
+		// Send it back
+		w.Write([]byte(query.Fen))
 	})
 
 	println("Server is running: http://localhost:8080")
