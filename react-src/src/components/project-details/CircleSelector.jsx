9bb0a972128938350d74de7e43f9623ac4f36574
Daniel Piper <piperdaniel1@gmail.com>
2023-04-23 18:20:18 -0700

    Fixed line position in CircleSelector

--- react-src/src/components/project-details/CircleSelector.jsx
+++ react-src/src/components/project-details/CircleSelector.jsx
@@ -4,43 +4,43 @@ import { TbLetterW } from 'react-icons/tb';
 import './CircleSelector.css';
 
 const CircleSelector = (props) => {
-  const handleClick = (value) => {
-    props.setSelected(value);
-  };
+    const handleClick = (value) => {
+        props.setSelected(value);
+    };
 
-  const circleClass = (value) => {
-    return props.selected === value ? 'circle selected' : 'circle';
-  };
+    const circleClass = (value) => {
+        return props.selected === value ? 'circle selected' : 'circle';
+    };
 
-  const iconClass = (value) => {
-    return props.selected === value ? 'discord-icon selected' : 'discord-icon';
-  };
+    const iconClass = (value) => {
+        return props.selected === value ? 'discord-icon selected' : 'discord-icon';
+    };
 
-  return (
-    <div className="circle-container">
-      <div className="circle-wrapper">
-        <div className={circleClass('Discord Bot')} onClick={() => handleClick('Discord Bot')}>
-          <FaDiscord className={iconClass('Discord Bot')} />
+    return (
+        <div className="circle-container">
+            <div className="circle-wrapper">
+                <div className={circleClass('Discord Bot')} onClick={() => handleClick('Discord Bot')}>
+                    <FaDiscord className={iconClass('Discord Bot')} />
+                </div>
+                <div className={circleClass('Wordle Bot')} onClick={() => handleClick('Wordle Bot')}>
+                    <TbLetterW className={iconClass('Wordle Bot')} />
+                </div>
+                <div className={circleClass('Chess Bot')} onClick={() => handleClick('Chess Bot')}>
+                    <FaChessBishop className={iconClass('Chess Bot')} />
+                </div>
+                <div className="line">
+                    <div className={`line-section ${props.selected === 'Discord Bot' ? 'selected' : ''}`} />
+                    <div className={`line-section ${props.selected === 'Wordle Bot' ? 'selected' : ''}`} />
+                    <div className={`line-section ${props.selected === 'Chess Bot' ? 'selected' : ''}`} />
+                </div>
+            </div>
+            <div className="circle-labels">
+                <span>Discord Bot</span>
+                <span>Wordle Bot</span>
+                <span>Chess Bot</span>
+            </div>
         </div>
-        <div className={circleClass('Wordle Bot')} onClick={() => handleClick('Wordle Bot')}>
-          <TbLetterW className={iconClass('Wordle Bot')} />
-        </div>
-        <div className={circleClass('Chess Bot')} onClick={() => handleClick('Chess Bot')}>
-          <FaChessBishop className={iconClass('Chess Bot')} />
-        </div>
-      </div>
-      <div className="line">
-        <div className={`line-section ${props.selected === 'Discord Bot' ? 'selected' : ''}`} />
-        <div className={`line-section ${props.selected === 'Wordle Bot' ? 'selected' : ''}`} />
-        <div className={`line-section ${props.selected === 'Chess Bot' ? 'selected' : ''}`} />
-      </div>
-      <div className="circle-labels">
-          <span>Discord Bot</span>
-          <span>Wordle Bot</span>
-          <span>Chess Bot</span>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CircleSelector;
